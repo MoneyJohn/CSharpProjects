f31542ca6806bd8e70664752dedd17b1a4964820
John Money <94144017+MoneyJohn@users.noreply.github.com>
2025-10-21 00:03:57 -0700

    test2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Write your first C# code msLearn
 Console.WriteLine("This is the first line.");
 Console.Write("This is ");
 Console.Write("the second ");
-Console.Write("line."); 
+Console.WriteLine("line.");
+
+//Store and retrieve data uning literal and variable vaues in C#
+
+//6-9 digits float (add f)
+//15-17 digits double (add d)
+//28-29 digits decimal (add m)
+
+Console.WriteLine(0.66453m);
+
+
+
